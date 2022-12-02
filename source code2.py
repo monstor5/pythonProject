a6034e25406bff627d4843e07a8792c4eb199369
kill <pea20102010@gmail.com>
2022-12-02 10:17:56 +0700

    myne

--- source code2.py
+++ source code2.py
@@ -1,66 +1,19 @@
+
 import PySimpleGUI as sg
-import re
-import hashlib
-def hash(fname, algo):
-    if algo == 'MD5':
-        hash = hashlib.md5()
-    elif algo == 'SHA1':
-        hash = hashlib.sha1()
-    elif algo == 'SHA256':
-        hash = hashlib.sha256()
-    with open(fname) as handle: #opening the file one line at a time for memory considerations
-        for line in handle:
-            hash.update(line.encode(encoding = 'utf-8'))
-    return(hash.hexdigest())
+
+
 layout = [
     [sg.Text('File 1'), sg.InputText(), sg.FileBrowse(),
-     sg.Checkbox('MD5'), sg.Checkbox('SHA1')
-     ],
-    [sg.Text('File 2'), sg.InputText(), sg.FileBrowse(),
-     sg.Checkbox('SHA256')
+     sg.Checkbox('Python'), sg.Checkbox('01(binary code)')
      ],
     [sg.Output(size=(88, 20))],
     [sg.Submit(), sg.Cancel()]
 ]
+
 window = sg.Window('File Compare', layout)
 while True:                             # The Event Loop
     event, values = window.read()
     # print(event, values) #debug
     if event in (None, 'Exit', 'Cancel'):
         break
-    if event == 'Submit':
-        file1 = file2 = isitago = None
-        # print(values[0],values[3])
-        if values[0] and values[3]:
-            file1 = re.findall('.+:\/.+\.+.', values[0])
-            file2 = re.findall('.+:\/.+\.+.', values[3])
-            isitago = 1
-            if not file1 and file1 is not None:
-                print('Error: File 1 path not valid.')
-                isitago = 0
-            elif not file2 and file2 is not None:
-                print('Error: File 2 path not valid.')
-                isitago = 0
-            elif values[1] is not True and values[2] is not True and values[4] is not True:
-                print('Error: Choose at least one type of Encryption Algorithm')
-            elif isitago == 1:
-                print('Info: Filepaths correctly defined.')
-                algos = [] #algos to compare
-                if values[1] == True: algos.append('MD5')
-                if values[2] == True: algos.append('SHA1')
-                if values[4] == True: algos.append('SHA256')
-                filepaths = [] #files
-                filepaths.append(values[0])
-                filepaths.append(values[3])
-                print('Info: File Comparison using:', algos)
-                for algo in algos:
-                    print(algo, ':')
-                    print(filepaths[0], ':', hash(filepaths[0], algo))
-                    print(filepaths[1], ':', hash(filepaths[1], algo))
-                    if hash(filepaths[0],algo) == hash(filepaths[1],algo):
-                        print('Files match for ', algo)
-                    else:
-                        print('Files do NOT match for ', algo)
-        else:
-            print('Please choose 2 files.')
-window.close()
+
