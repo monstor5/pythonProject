48964b70ffb48b0003627653917cb24283ae1d9d
monstor5 <zhoratazeev@gmail.com>
2022-12-11 10:59:03 +0700

    error

--- 1co1/sourceCode.py
+++ 1co1/sourceCode.py
@@ -1,3 +1,10 @@
 ## находить фаил
 import os
 from sourceCode2 import pictures
+from ngf import Binari_cod
+
+br = Binari_cod()
+t = int(input("Двоичный код, будет приобрачовываться в текст с двоичного кода: "))
+p = ("Текст, будем преобразовывать в двоичный код: ")
+print(br.text_from_bits(bits = t))
+print(br.text_to_bits1(text = p))
