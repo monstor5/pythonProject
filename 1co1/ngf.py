af81acada7740b83f6f94f68b854b4e06817a982
monstor5 <zhoratazeev@gmail.com>
2022-12-11 11:00:52 +0700

    error

--- 1co1/ngf.py
+++ 1co1/ngf.py
@@ -6,6 +6,6 @@ class Binari_cod:
   def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
       n = int(bits, 2)
       return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
-
+p
 
 
